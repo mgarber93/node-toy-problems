257bcea5cf5385c30d3f9f6e389db9b6534c93e5
mgarber93 <mgarber93@gmail.com>
2022-10-05 20:56:19 -0400

    Extend test coverage

--- src/scramble.test.js
+++ src/scramble.test.js
@@ -1,10 +1,22 @@
-const {scramble} = require('./scramble');
+const { scramble } = require('./scramble');
 
 
 describe.each([
   ['rkqodlw', 'world', true],
   ['cedewaraaossoqqyt', 'codewars', true],
-  ['katas', 'steak', false]
+  ['katas', 'steak', false],
+  ['rkqodlw', 'world', true],
+  ['cedewaraaossoqqyt', 'codewars', true],
+  ['katas', 'steak', false],
+  ['scriptjavx', 'javascript', false],
+  ['scriptingjava', 'javascript', true],
+  ['scriptsjava', 'javascripts', true],
+  ['javscripts', 'javascript', false],
+  ['jscripts', 'javascript', false],
+  ['aabbcamaomsccdd', 'commas', true],
+  ['commas', 'commas', true],
+  ['sammoc', 'commas', true]
+
 ])('scramble(%s, %s)', (str1, str2, expected) => {
   test(`returns ${expected}`, () => {
     expect(scramble(str1, str2)).toEqual(expected);
